3cae0914154e602961cccc3b2dc497e0ca6ace27
Dave Jones <dave.jones@thehutgroup.com>
2022-05-24 16:37:55 +0100

    Added flag checker + gitignore

--- BaseConverter.h
+++ BaseConverter.h
@@ -4,10 +4,10 @@
 #include <math.h>
 
 int binToDec(char *valToConv, char **retPtr);
-void decToBin(char *valToConv, char **retPtr);
-void binToHex(char *valToConv, char **retPtr);
-void hexToBin(char *valToConv, char **retPtr);
-void decToHex(char *valToConv, char **retPtr);
+int decToBin(char *valToConv, char **retPtr);
+int binToHex(char *valToConv, char **retPtr);
+int hexToBin(char *valToConv, char **retPtr);
+int decToHex(char *valToConv, char **retPtr);
 int hexToDec(char *valToConv, char **retPtr);
 void stringFromLong(long resultAsLong, int sizeOfReturnString, char *retVal);
 int intFromNonDecimal(int constant, int base, int powerModifier, int inputSize);
